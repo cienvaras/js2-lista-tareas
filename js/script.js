69479e8ee65e67eaf6050ad4745911d1507b3944
Andrés Díaz Soto <andres.diaz.soto@gmail.com>
2020-10-26 21:48:41 -0600

    Get tasks from API

--- js/script.js
+++ js/script.js
@@ -2,52 +2,47 @@
 // Lista de tareas
 //
 
+/* eslint-disable no-underscore-dangle, no-plusplus */
+
 //
 // Modelo.
 //
 
+// API User ID.
+const uid = 1;
 // Contador de tareas (para asignar un id único a cada tarea).
 let contadorTareas = 0;
 // Lista de tareas (Array).
 let tareas = [];
-// Trata de obtener la lista de tareas de localStorage,
-// si el resultado es distinto de 'null', usa las tareas almacenadas.
-const datosLocalStorage = localStorage.getItem('tareas');
-if (datosLocalStorage) {
-  tareas = JSON.parse(datosLocalStorage);
-}
 
 // Se lee el contador de tareas del localStorage.
 const contadorLocalStorage = localStorage.getItem('contador');
-console.log(contadorLocalStorage);
-
-console.log(tareas);
 
 if (contadorLocalStorage) {
-  contadorTareas = parseInt(contadorLocalStorage);
+  contadorTareas = parseInt(contadorLocalStorage, 10);
 }
 
 // addTask(): Agrega una tarea en la lista.
 function addTask(nombreTarea, fechaTarea, completoTarea) {
   // Crea un objeto que representa la nueva tarea.
   const nuevaTarea = {
-    id: contadorTareas,
-    nombre: nombreTarea,
-    completo: completoTarea,
-    fecha: fechaTarea,
+    _id: contadorTareas,
+    name: nombreTarea,
+    complete: completoTarea,
+    date: fechaTarea,
   };
 
   // Agrega el objeto en el array.
   tareas.push(nuevaTarea);
 
   // Incrementa el contador de tareas.
-  contadorTareas++;
+  contadorTareas += 1;
   // Se guarda el contador de tareas en localStorage.
   localStorage.setItem('contador', contadorTareas);
 
   // Agrega la tarea al DOM.
-  appendTaskDOM(nuevaTarea);
-  
+  appendTaskDOM(nuevaTarea); // eslint-disable-line no-use-before-define
+
   // Guarda la lista de tareas en localStorage.
   localStorage.setItem('tareas', JSON.stringify(tareas));
 }
@@ -57,8 +52,8 @@ function taskStatus(id, complete) {
   // Recorre la lista de tareas.
   for (let i = 0; i < tareas.length; i++) {
     // Cuando encuentra la tarea con el id correcto cambia su estado.
-    if (tareas[i].id === id) {
-      tareas[i].completo = complete;
+    if (tareas[i]._id === id) {
+      tareas[i].complete = complete;
       break;
     }
   }
@@ -71,7 +66,7 @@ function deleteTask(id) {
   // Recorre la lista de tareas.
   for (let i = 0; i < tareas.length; i++) {
     // Cuando encuentra la tarea con el id correcto la borra.
-    if (tareas[i].id === id) {
+    if (tareas[i]._id === id) {
       tareas.splice(i, 1);
       break;
     }
@@ -94,16 +89,16 @@ function appendTaskDOM(tarea) {
   // Checkbox.
   const checkbox = document.createElement('input');
   checkbox.setAttribute('type', 'checkbox');
-  checkbox.setAttribute('id', `tarea-${tarea.id}`);
-  checkbox.checked = tarea.completo;
+  checkbox.setAttribute('id', `tarea-${tarea._id}`);
+  checkbox.checked = tarea.complete;
   // Label.
   const label = document.createElement('label');
-  label.setAttribute('for', `tarea-${tarea.id}`);
-  label.innerHTML = `${tarea.nombre} - ${tarea.fecha}`;
+  label.setAttribute('for', `tarea-${tarea._id}`);
+  label.innerHTML = `${tarea.name} - ${tarea.date}`;
   // Botón de borrar.
   const buttonDelete = document.createElement('button');
   buttonDelete.className = 'task-list__delete';
-  buttonDelete.setAttribute('id', `delete-${tarea.id}`);
+  buttonDelete.setAttribute('id', `delete-${tarea._id}`);
   buttonDelete.innerHTML = 'Borrar';
   // Se agregan elementos.
   item.appendChild(checkbox);
@@ -114,13 +109,13 @@ function appendTaskDOM(tarea) {
   checkbox.addEventListener('click', (event) => {
     const complete = event.currentTarget.checked;
     const itemId = event.currentTarget.getAttribute('id');
-    const taskId = parseInt(itemId.substring(6));
+    const taskId = parseInt(itemId.substring(6), 10);
     taskStatus(taskId, complete);
   });
   // Evento para borrar tareas.
   buttonDelete.addEventListener('click', (event) => {
     const itemId = event.currentTarget.getAttribute('id');
-    const taskId = parseInt(itemId.substring(7));
+    const taskId = parseInt(itemId.substring(7), 10);
     deleteTask(taskId);
     // Borra la tarea en el DOM.
     event.currentTarget.parentNode.remove();
@@ -128,9 +123,14 @@ function appendTaskDOM(tarea) {
 }
 
 // Inicialización de la lista del DOM, a partir de las tareas existentes.
-for (let i = 0; i < tareas.length; i++) {
-  appendTaskDOM(tareas[i]);
-}
+fetch(`https://js2-tareas-api.netlify.app/api/tareas?uid=${uid}`)
+  .then((response) => response.json())
+  .then((data) => {
+    tareas = data;
+    for (let i = 0; i < tareas.length; i++) {
+      appendTaskDOM(tareas[i]);
+    }
+  });
 
 //
 // Controlador.
@@ -151,4 +151,4 @@ formulario.addEventListener('submit', (event) => {
   // Reseteamos el form.
   formulario.elements[0].value = '';
   formulario.elements[1].value = '';
-})
+});
